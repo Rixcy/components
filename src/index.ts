a96d91f056149faff864b5dab181833085aae745
Rixcy <admin@rickbooth.me>
2020-09-27 14:40:51 +0100

    feat: add navbar component

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 export * from './Alert'
 export * from './Button'
 export * from './Pagination'
+export * from './Navbar'
 export * from './ProgressBar'
 export * from './SimpleFooter'
 export * from './Tooltip'
